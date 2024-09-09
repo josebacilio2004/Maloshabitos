77b60a8f73cdc20933bf5240c1fea22151600362
JoseBacilio <74934503@continental.edu.pe>
2024-09-09 18:59:47 -0500

    Refactorizacion ejem03

--- Ejemploo3.py
+++ Ejemploo3.py
@@ -1,23 +1,24 @@
 # Función para calcular el área de un rectángulo
-def f(a, b):
-    result = a * b
-    return result
+def CalcularAreaRectangulo(Altura, Base):
+    areaRectangulo = Altura * Base
+    return areaRectangulo
+
 
 # Función para calcular el área de un triángulo
-def g(b, h):
-    r = 0.5 * b * h
-    return r
+def CalcularAreaTriangulo(Base, Altura):
+    areaTriangulo = 0.5 * Base * Altura
+    return areaTriangulo
+
 
 # Función principal
-def main():
-    x = 4
-    y = 6
-    rect_area = f(x, y)
+if __name__ == "__main__":
+    dimension1Rect = float(input("Dimensión 1 Rectángulo: "))
+    dimension2Rect = float(input("Dimensión 2 Rectángulo: "))
+    dimension3Tri = float(input("Dimensión 1 Triángulo: "))
+    dimension4Tri = float(input("Dimensión 2 Triángulo: "))
+
+    rect_area = CalcularAreaRectangulo(dimension1Rect, dimension2Rect)
     print("Área del rectángulo:", rect_area)
 
-    base = 5
-    altura = 8
-    tri_area = g(base, altura)
+    tri_area = CalcularAreaTriangulo(dimension3Tri, dimension4Tri)
     print("Área del triángulo:", tri_area)
-
-main()
